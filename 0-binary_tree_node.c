079e38f33f78804f5db652f1afa9630d3ba27f69
AzorAha1 <wizfaiz@icloud.com>
2023-11-28 18:37:27 +0100

    update

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	newnode->parent = parent;
 	if (parent == NULL)
 	{
-		newnode->n = value
+		newnode->n = value;
 		return (newnode);
 	}
 	newnode->n = value;
