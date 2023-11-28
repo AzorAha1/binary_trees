91466ca14f4adbafbb9a1a0debc3b0f63d01ad18
AzorAha1 <wizfaiz@icloud.com>
2023-11-28 17:47:39 +0100

    0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+* binary_tree_node - function name
+* @parent: parent of the tree
+* @value: value
+* description - create a binary tree node
+* Return: binary tree node pointer returned
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!newnode)
+	{
+		return (NULL);
+	}
+	newnode->parent = parent;
+	newnode->n = value;
+	return (newnode);
+}
