1227e8b39c538fc30c5ac2af5daf55aa0a31cc8e
NeneNafe <nenechiwaya@gmail.com>
2023-11-29 20:50:22 +0200

    Everything

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,9 +8,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftHeight = 0, rightHeight = 0;
+
 	if (!tree)
 		return (0);
 
-		return (1);
-	return (0);
+	leftHeight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rightHeight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (leftHeight < rightHeight ? leftHeight : rightHeight);
 }
