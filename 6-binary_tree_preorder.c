ee9dc26941cec287df40760cf03a4a9bd27f6427
AzorAha1 <wizfaiz@icloud.com>
2023-11-29 14:49:13 +0100

    6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_preorder -  a function that goes through a binary tree
+* @tree: a pointer to struct
+* @func: the function that calls each node
+* description - description
+* Return: nothing
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+	{
+		return;
+	}
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+	binary_tree_preorder(tree->left, func);
+}
