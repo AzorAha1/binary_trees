ef6996a3ea740e117df36eea82318be70d8ec452
AzorAha1 <wizfaiz@icloud.com>
2023-11-30 14:53:13 +0100

    16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,19 +29,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree)
 	{
-		return (0);
+		return (1);
 	}
 	leftheight = height(tree->left);
 	rightheight = height(tree->right);
 	if (leftheight != rightheight)
-	{
-		return (0);
-	}
-	if (tree->left && tree->right)
-	{
-		return (1);
-	}
-	if (tree->left || tree->right)
 	{
 		return (0);
 	}
