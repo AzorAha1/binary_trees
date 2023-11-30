29ef7f2495df4a530a0bd03743ec1f13e8308970
AzorAha1 <wizfaiz@icloud.com>
2023-11-30 13:22:08 +0100

    update

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,23 +1,5 @@
 #include "binary_trees.h"
 /**
-* binary_tree_balance - checks balance factor
-* @tree: Pointer to the root node
-* Return: 0 if tree is NULL.
-*/
-int height(const binary_tree_t *tree);
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	int heightofleft = height(tree->left);
-
-	int heightofright = height(tree->right);
-
-	int checkbalance = heightofleft - heightofright;
-
-	return (checkbalance);
-}
-/**
 * height - cal height
 * @tree: tree
 * description - description
@@ -30,5 +12,23 @@ int height(const binary_tree_t *tree)
 	int leftheight = height(tree->left);
 	int rightheight = height(tree->right);
 
-	return (1 + (leftheight > rightheight ? leftheight : rightheight));
+	return (1 + max(leftheight, rightheight));
 }
+/**
+* binary_tree_balance - checks balance factor
+* @tree: Pointer to the root node
+* Return: 0 if tree is NULL.
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	int heightofleft = height(tree->left);
+
+	int heightofright = height(tree->right);
+
+	int checkbalance = heightofleft - heightofright;
+
+	return (checkbalance);
+}
+
