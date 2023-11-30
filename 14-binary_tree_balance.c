d72ef8789c2ecde0c47de89ab000bf12c3a7dc68
AzorAha1 <wizfaiz@icloud.com>
2023-11-30 10:44:30 +0100

    adding height function to help get the balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
+/**
+* height - cal height
+* @tree: tree
+* description - description
+* Return: 0 if tree is null
+*/
+int height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	int leftheight = height(tree->left);
+	int rightheight = height(tree->right);
 
+	return (1 + max(leftheight, rightheight));
+}
 /**
 * binary_tree_balance - checks balance factor
 * @tree: Pointer to the root node
@@ -9,8 +23,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	int heightofleft = binary_tree_height(tree->left);
-	int heightofright = binary_tree_height(tree->right);
+	int heightofleft = height(tree->left);
+
+	int heightofright = height(tree->right);
+
 	int checkbalance = heightofleft - heightofright;
 
 	return (checkbalance);
